76b18efca4499e9e1c922461e58c6b1a4add3ed8
Alessio <randomcoil@protonmail.com>
2026-04-24 17:15:35 +0200

    Initial round of porting

--- README.md
+++ README.md
@@ -4,21 +4,83 @@
 
 **Note**: 
 Size of the protein sequence input file should be adjusted according to the memory size of your computer. 
-This source code was developed in Linux, and has been tested in Ubuntu 14.04 with Python 2.7, Python 3.4, Python 3.5 or Python 3.6. 
-It should be noted that Python 3.7 is currently not supported.
+This source code requires Python 3.9, 3.10, 3.11, 3.12, or 3.13.
 
 1. Clone the repository
 
         git clone https://bitbucket.org/kaistsystemsbiology/deepec.git
 
-2. Create and activate a conda environment
+2. Install DIAMOND (sequence alignment tool)
 
-        conda env create -f environment.yml
-        conda activate deepec
+    **Ubuntu/Debian:**
+    
+        sudo apt-get update
+        sudo apt-get install diamond-aligner
+
+    **macOS (using Homebrew):**
+    
+        brew install diamond
+
+    **Or download from:** https://github.com/bbuchfink/diamond
+
+3. Create a virtual environment (recommended)
+
+        python -m venv deepec_env
+        source deepec_env/bin/activate  # On Windows: deepec_env\Scripts\activate
+
+4. Upgrade pip
+
+        pip install --upgrade pip
+
+5. Install Python dependencies
+
+        pip install -r requirements.txt
+
+   If installation fails, install packages individually with pre-built wheels:
+
+        pip install tensorflow numpy scipy pandas scikit-learn h5py biopython
+
+**Important**: The pre-trained model files (.h5) and label encoders (.pkl) were created with Keras 2.1.6 and scikit-learn 0.19. The code includes compatibility layers to load these legacy files with modern TensorFlow 2.x and scikit-learn 1.3+.
 
 ##Example
 
 - Run DeepEC
 
-        python deepec.py -i ./example/test.fa -o ./output 
+        python deepec.py -i ./example/test.fa -o ./output
+
+##Verification (Optional)
+
+To verify that the upgraded version produces the same results as the original:
+
+**Automated Docker comparison (recommended):**
+
+        chmod +x test_comparison.sh
+        ./test_comparison.sh
+
+**Note:** Docker needs at least 4GB RAM allocated. If build fails with memory errors:
+- Docker Desktop → Settings → Resources → Memory → Increase to 6-8GB
+- Restart Docker Desktop and try again
+
+This script:
+1. Builds a Docker image with the original conda environment and old code
+2. Runs the legacy version inside Docker (outputs written to host via volume mount)
+3. Runs the new Python 3.13 version locally
+4. Compares the outputs with the comparison script
+
+**Manual Docker test:**
+
+        # Build legacy version container
+        docker build -f Dockerfile.legacy -t deepec:legacy .
+        
+        # Run legacy version (outputs to local directory)
+        docker run --rm -v "$(pwd):/data" deepec:legacy \
+            -i /data/example/test.fa -o /data/output_old
+        
+        # Run new version
+        python deepec.py -i example/test.fa -o output_new
+        
+        # Compare
+        python compare_outputs.py output_old output_new
+
+Expected: EC number predictions should match exactly. DNN activity scores may differ slightly (< 0.001) due to TensorFlow version differences. 
 
--- deepec/ec_prediction_dl.py
+++ deepec/ec_prediction_dl.py
@@ -1,7 +1,8 @@
 from Bio import SeqIO
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import numpy as np
 import pandas as pd
+import pickle
 import sys
 import os
 import glob
@@ -11,36 +12,80 @@ import time
 import subprocess
 import copy
 
-    
+
+# Compatibility layer for loading old scikit-learn pickle files
+class CompatibilityUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        # Remap old sklearn module paths to new ones
+        if module == "sklearn.preprocessing.label":
+            module = "sklearn.preprocessing._label"
+        return super().find_class(module, name)
+
+
+def load_pickle_compat(file_path):
+    """Load pickle file with compatibility for old scikit-learn versions"""
+    with open(file_path, "rb") as f:
+        return CompatibilityUnpickler(f).load()
+
+
 def preprocessing(fasta_file, temp_file):
     l = 1000
-    fp = open(temp_file, 'w')
+    fp = open(temp_file, "w")
     input_handle = open(fasta_file, "r")
-    for seq_record in SeqIO.parse(input_handle, "fasta"):   
+    for seq_record in SeqIO.parse(input_handle, "fasta"):
         seq_id = seq_record.id
         seq = seq_record.seq
         if len(seq) <= 1000:
-            fp.write('>%s\n'%(seq_id))
-            fp.write('%s\n'%(seq.strip()))
+            fp.write(">%s\n" % (seq_id))
+            fp.write("%s\n" % (seq.strip()))
         else:
-            for i in range(0, len(seq)-l+1, 100):
-                new_seq_id = '%s_SEPARATED_SEQUENCE_(%s_%s)' % (seq_id, i+1, i+l+1)
-                new_seq = seq[i:i+l]
-                fp.write('>%s\n'%(new_seq_id))
-                fp.write('%s\n'%(new_seq))
-    
+            for i in range(0, len(seq) - l + 1, 100):
+                new_seq_id = "%s_SEPARATED_SEQUENCE_(%s_%s)" % (
+                    seq_id,
+                    i + 1,
+                    i + l + 1,
+                )
+                new_seq = seq[i : i + l]
+                fp.write(">%s\n" % (new_seq_id))
+                fp.write("%s\n" % (new_seq))
+
     input_handle.close()
     fp.close()
     return
 
+
 def fill_aa(seq):
     fill_aa_cnt = 1000 - len(seq)
-    add_aa_seq = '_' * fill_aa_cnt
+    add_aa_seq = "_" * fill_aa_cnt
     new_seq = seq + add_aa_seq
     return new_seq
 
+
 def score_info():
-    aa_list = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'X', '_']
+    aa_list = [
+        "A",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "K",
+        "L",
+        "M",
+        "N",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "V",
+        "W",
+        "Y",
+        "X",
+        "_",
+    ]
     aa_score_info = {}
     for aa in aa_list:
         for aa2 in aa_list:
@@ -52,23 +97,47 @@ def score_info():
                 aa_score_info[(aa2, aa)] = 0.0
     return aa_score_info
 
+
 def one_hot_encoding(seq, aa_score_info):
     data = np.zeros((1000, 21), dtype=np.float32)
-    aa_list = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'X']
+    aa_list = [
+        "A",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "K",
+        "L",
+        "M",
+        "N",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "V",
+        "W",
+        "Y",
+        "X",
+    ]
     for i, aa in enumerate(seq):
         for j, aa2 in enumerate(aa_list):
             data[i, j] = aa_score_info[aa, aa2]
     return data
 
+
 def run_one_hot_encoding(fasta_file, temp_file):
     aa_score_info = score_info()
-    fp = open(temp_file, 'w')
-    feature_names = ['Feature%s'%(i) for i in range(1, 21001)] 
-    
-    fp.write('%s\n'%(','.join(['ID']+feature_names)))
-    
+    fp = open(temp_file, "w")
+    feature_names = ["Feature%s" % (i) for i in range(1, 21001)]
+
+    fp.write("%s\n" % (",".join(["ID"] + feature_names)))
+
     input_handle = open(fasta_file, "r")
-    for seq_record in SeqIO.parse(input_handle, "fasta"):   
+    for seq_record in SeqIO.parse(input_handle, "fasta"):
         try:
             seq_id = seq_record.id
             seq = seq_record.seq
@@ -77,8 +146,10 @@ def run_one_hot_encoding(fasta_file, temp_file):
                     seq = fill_aa(seq)
                 encoded_vector = one_hot_encoding(seq, aa_score_info)
                 flatten_encoded_vector = encoded_vector.flatten()
-                flatten_encoded_vector_str = [str(each_val) for each_val in flatten_encoded_vector]
-                fp.write('%s\n'%(','.join([seq_id]+flatten_encoded_vector_str)))
+                flatten_encoded_vector_str = [
+                    str(each_val) for each_val in flatten_encoded_vector
+                ]
+                fp.write("%s\n" % (",".join([seq_id] + flatten_encoded_vector_str)))
         except:
             pass
     input_handle.close()
@@ -92,24 +163,25 @@ def predict_target_ec(df, output_file, DeepEC_model):
     for i in range(len(X_temp)):
         temp = np.reshape(X_temp[i], (1000, 21))
         new_X.append(temp)
-    
+
     X = np.asarray(new_X)
     X = X.reshape(X.shape[0], 1000, 21, 1)
-    
-    model = load_model(DeepEC_model)
-    
+
+    model = load_model(DeepEC_model, compile=False)
+
     y_predicted = model.predict(X)
     enzyme_list = []
-    with open(output_file, 'w') as fp:
-        fp.write('Query ID\tPredicted class\tDNN activity\n')
-        for i in range(len(y_predicted)): 
+    with open(output_file, "w") as fp:
+        fp.write("Query ID\tPredicted class\tDNN activity\n")
+        for i in range(len(y_predicted)):
             socre = y_predicted[i][1]
             if y_predicted[i][1] > 0.5:
                 enzyme_list.append(seq_ids[i])
-                fp.write('%s\t%s\t%s\n'%(seq_ids[i], 'Target', socre))
+                fp.write("%s\t%s\t%s\n" % (seq_ids[i], "Target", socre))
             else:
-                fp.write('%s\t%s\t%s\n'%(seq_ids[i], 'Non-target', 1-socre))
+                fp.write("%s\t%s\t%s\n" % (seq_ids[i], "Non-target", 1 - socre))
     return enzyme_list
+
 
 def predict_enzyme(df, output_file, DeepEC_model):
     seq_ids = list(df.index)
@@ -118,57 +190,52 @@ def predict_enzyme(df, output_file, DeepEC_model):
     for i in range(len(X_temp)):
         temp = np.reshape(X_temp[i], (1000, 21))
         new_X.append(temp)
-    
+
     X = np.asarray(new_X)
     X = X.reshape(X.shape[0], 1000, 21, 1)
-    
-    model = load_model(DeepEC_model)
-    
+
+    model = load_model(DeepEC_model, compile=False)
+
     y_predicted = model.predict(X)
     enzyme_list = []
-    with open(output_file, 'w') as fp:
-        fp.write('Query ID\tPredicted class\tDNN activity\n')
-        for i in range(len(y_predicted)): 
+    with open(output_file, "w") as fp:
+        fp.write("Query ID\tPredicted class\tDNN activity\n")
+        for i in range(len(y_predicted)):
             socre = y_predicted[i][1]
             if y_predicted[i][1] > 0.5:
                 enzyme_list.append(seq_ids[i])
-                fp.write('%s\t%s\t%s\n'%(seq_ids[i], 'Enzyme', socre))
+                fp.write("%s\t%s\t%s\n" % (seq_ids[i], "Enzyme", socre))
             else:
-                fp.write('%s\t%s\t%s\n'%(seq_ids[i], 'Non-enzyme', 1-socre))
+                fp.write("%s\t%s\t%s\n" % (seq_ids[i], "Non-enzyme", 1 - socre))
     return enzyme_list
 
+
 def predict_ec(df, output_file, DeepEC_model, MultiLabelBinarizer, threshold=0.5):
-    if (sys.version_info > (3, 0)):
-        import _pickle as cPickle
-    else:
-        import cPickle
-    
     seq_ids = list(df.index)
     X_temp = df.values
     new_X = []
     for i in range(len(X_temp)):
         temp = np.reshape(X_temp[i], (1000, 21))
         new_X.append(temp)
-    
+
     X = np.asarray(new_X)
     X = X.reshape(X.shape[0], 1000, 21, 1)
-    
-    with open(MultiLabelBinarizer, 'rb') as fid:
-        lb = cPickle.load(fid)
-    
-    model = load_model(DeepEC_model)
-    
+
+    lb = load_pickle_compat(MultiLabelBinarizer)
+
+    model = load_model(DeepEC_model, compile=False)
+
     y_predicted = model.predict(X)
     original_y_predicted = copy.deepcopy(y_predicted)
-    
+
     y_predicted[y_predicted >= threshold] = 1
-    y_predicted[y_predicted <threshold] = 0
+    y_predicted[y_predicted < threshold] = 0
 
-    y_predicted_results = lb.inverse_transform(y_predicted) 
+    y_predicted_results = lb.inverse_transform(y_predicted)
 
-    with open(output_file, 'w') as fp:
-        fp.write('Query ID\tPredicted EC number\tDNN activity\n')
-        for i in range(len(y_predicted)): 
+    with open(output_file, "w") as fp:
+        fp.write("Query ID\tPredicted EC number\tDNN activity\n")
+        for i in range(len(y_predicted)):
             each_y_predicted = copy.deepcopy(y_predicted[i])
             predicted_ddi_score = original_y_predicted[i]
             target_index = np.flatnonzero(each_y_predicted == 1)
@@ -179,15 +246,13 @@ def predict_ec(df, output_file, DeepEC_model, MultiLabelBinarizer, threshold=0.5
                     each_y_predicted[each_idx] = 1
 
                     each_y_predicted = np.array([list(each_y_predicted)])
-                    y_transformed = lb.inverse_transform(each_y_predicted) 
+                    y_transformed = lb.inverse_transform(each_y_predicted)
 
                     score = predicted_ddi_score[each_idx]
                     ec_number = y_transformed[0][0]
-                    fp.write('%s\t%s\t%s\n'%(seq_ids[i], ec_number, score))
-            
+                    fp.write("%s\t%s\t%s\n" % (seq_ids[i], ec_number, score))
+
             else:
-                fp.write('%s\t%s\t%s\n'%( seq_ids[i], 'EC number not predicted', 'N/A'))
-
-
-
-
+                fp.write(
+                    "%s\t%s\t%s\n" % (seq_ids[i], "EC number not predicted", "N/A")
+                )
